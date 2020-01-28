64bed81f6eb3c2b04a61b28f7d891eabd6e2adc9
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-01-28 20:56:45 +0100

    Lista de productos.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const products = [
+    {nombre: "Microondas", precio: 45, existencias: 6},
+    {nombre: "Frigorífico", precio: 200, existencias: 4},
+    {nombre: "Lámpara", precio: 20, existencias: 14},
+    {nombre: "TV", precio: 90, existencias:0},
+    {nombre: "Lavadora", precio: 290, existencias:3},
+    {nombre: "Secadora", precio: 180, existencias:5}
+  ];
+
+  res.render('index', { title: 'The Jungle', products });
 });
 
 module.exports = router;
