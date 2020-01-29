fbffcfa49e17a81dfe56e584b50db9a4ceb50c23
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-01-29 19:35:05 +0100

    Refactorización: productos en un módulo aparte.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var products = require('../models/products.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  const products = [
-    {nombre: "Microondas", precio: 45, existencias: 6, imagen: "microondas.jpg"},
-    {nombre: "Frigorífico", precio: 200, existencias: 4, imagen: "frigorifico.jpg"},
-    {nombre: "Lámpara", precio: 20, existencias: 14, imagen: "lampara.jpg"},
-    {nombre: "TV", precio: 90, existencias:0, imagen: "tv.jpg"},
-    {nombre: "Lavadora", precio: 290, existencias:3, imagen: "lavadora.jpg"},
-    {nombre: "Secadora", precio: 180, existencias:5, imagen: "secadora.jpg"}
-  ];
-
   res.render('index', { title: 'The Jungle', products });
 });
 
