c97c76eaef6c67c5d4776bc37a697fb77439ca4d
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-01-29 18:32:52 +0100

    Imágenes de muestra.

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   
   const products = [
-    {nombre: "Microondas", precio: 45, existencias: 6},
-    {nombre: "Frigorífico", precio: 200, existencias: 4},
-    {nombre: "Lámpara", precio: 20, existencias: 14},
-    {nombre: "TV", precio: 90, existencias:0},
-    {nombre: "Lavadora", precio: 290, existencias:3},
-    {nombre: "Secadora", precio: 180, existencias:5}
+    {nombre: "Microondas", precio: 45, existencias: 6, imagen: "microondas.jpg"},
+    {nombre: "Frigorífico", precio: 200, existencias: 4, imagen: "frigorifico.jpg"},
+    {nombre: "Lámpara", precio: 20, existencias: 14, imagen: "lampara.jpg"},
+    {nombre: "TV", precio: 90, existencias:0, imagen: "tv.jpg"},
+    {nombre: "Lavadora", precio: 290, existencias:3, imagen: "lavadora.jpg"},
+    {nombre: "Secadora", precio: 180, existencias:5, imagen: "secadora.jpg"}
   ];
 
   res.render('index', { title: 'The Jungle', products });
